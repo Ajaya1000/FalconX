5bacc4a6b8f6532d5c2dd9be036bd272be200ca1
Ajaya Mati <ajaya.mati@nobroker.in>
2023-09-07 11:23:05 +0530

    - added services

---
+++ FalconX/DependencyInjection/Container+Services.swift
@@ -0,0 +1,17 @@
+//
+//  Container+Services.swift
+//  FalconX
+//
+//  Created by Ajaya Mati on 06/09/23.
+//
+
+import Foundation
+import Swinject
+import SwinjectAutoregistration
+
+extension Container {
+    func registerServices() {
+        self.autoregister(SessionService.self, initializer: SessionService.init).inObjectScope(.container)
+        self.autoregister(NetworkManager.self, initializer: NetworkManager.init).inObjectScope(.container)
+    }
+}
